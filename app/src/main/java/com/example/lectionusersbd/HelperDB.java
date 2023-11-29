6c88c4094e0359fb4120fc2fc10d6a9ca9deb688
OtsokovShA <shamil24@mail.ru>
2023-11-29 09:07:42 +0300

    Add helperDB class

---
+++ app/src/main/java/com/example/lectionusersbd/HelperDB.java
@@ -0,0 +1,38 @@
+package com.example.lectionusersbd;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class HelperDB extends SQLiteOpenHelper {
+    private static final String DB_NAME = "users.db"; // Имя базы данных
+    private static final int DB_VERSION = 1; // Версия базы данных
+    public  static final String tableName = "user";
+    public static final String login = "login";
+    public static final String passw = "password";
+    public static final String email = "email";
+    public static final String phone = "phone";
+
+    public HelperDB(@Nullable Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL("CREATE TABLE "+ tableName+" ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + login+" TEXT, "
+                + passw+" TEXT, "
+                + email+" TEXT, "
+                + phone+" TEXT);");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        String SQL_Delete = "drop table if exists "+tableName;
+        sqLiteDatabase.execSQL(SQL_Delete);
+        onCreate(sqLiteDatabase);
+    }
+}
--- app/src/main/java/com/example/lectionusersbd/RegistationActivity.java
+++ app/src/main/java/com/example/lectionusersbd/RegistationActivity.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistationActivity extends AppCompatActivity {
+    EditText edLogin, edPassword, edMail, edPhone;
+    User user;
+    Button btnReg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,27 @@ public class RegistationActivity extends AppCompatActivity {
     }
 
     public void onClick(View view) {
+        String strLogin = edLogin.getText().toString().trim();
+        String strPassword = edPassword.getText().toString().trim();
+        String strEmail = edMail.getText().toString().trim();
+        String strPhone = edPhone.getText().toString().trim();
+        edLogin.setText("");
+        edPassword.setText("");
+        edPhone.setText("");
+        edMail.setText("");
+        user = new User(strLogin,strPassword,strEmail,strPhone);
+     //   if (isEmptyDB()) {
+     //       goRegistr();
+     //       return;
+     //   }
+     //   if (isFoundUser(strLogin, strPassword))
+     //   {
+     //       Toast.makeText(this, "Пользователь с такими данными зарегистрирован!", Toast.LENGTH_SHORT).show();
+     //       return;
+     //   }
+        goRegistr();
+    }
+
+    private void goRegistr() {
     }
 }
