a4e2a3160e4a5d3c0f8e63633ee009730dcca001
OtsokovShA <shamil24@mail.ru>
2023-11-29 09:19:03 +0300

    RegistrationActivity class работу с базой данных

--- app/src/main/java/com/example/lectionusersbd/RegistationActivity.java
+++ app/src/main/java/com/example/lectionusersbd/RegistationActivity.java
@@ -2,6 +2,9 @@ package com.example.lectionusersbd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +16,8 @@ public class RegistationActivity extends AppCompatActivity {
     User user;
     Button btnReg;
 
+    SQLiteDatabase sqDb;
+    HelperDB myDb;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,18 +34,58 @@ public class RegistationActivity extends AppCompatActivity {
         edPhone.setText("");
         edMail.setText("");
         user = new User(strLogin,strPassword,strEmail,strPhone);
-     //   if (isEmptyDB()) {
-     //       goRegistr();
-     //       return;
-     //   }
-     //   if (isFoundUser(strLogin, strPassword))
-     //   {
-     //       Toast.makeText(this, "Пользователь с такими данными зарегистрирован!", Toast.LENGTH_SHORT).show();
-     //       return;
-     //   }
+        if (isEmptyDB()) {
+            goRegistr();
+            return;
+        }
+        if (isFoundUser(strLogin, strPassword))
+        {
+            Toast.makeText(this, "Пользователь с такими данными зарегистрирован!", Toast.LENGTH_SHORT).show();
+            return;
+        }
         goRegistr();
     }
 
+    private boolean isEmptyDB() {
+        sqDb = myDb.getWritableDatabase();
+        Cursor c = sqDb.query(HelperDB.tableName, null,null, null, null, null, null);
+        int count = c.getCount();
+        c.close();
+        return (count==0);
+    }
+
     private void goRegistr() {
+        ContentValues cv = new ContentValues();
+        cv.put(HelperDB.login, this.user.getLogin());
+        cv.put(HelperDB.passw, this.user.getPassword());
+        cv.put(HelperDB.email, this.user.getEmail());
+        cv.put(HelperDB.phone, this.user.getPhone());
+        sqDb = myDb.getWritableDatabase();
+        sqDb.insert(HelperDB.tableName, null, cv);
+        sqDb.close();
+    }
+
+    private boolean isFoundUser(String strLogin, String strPassword) {
+        boolean flag = false;
+        sqDb = myDb.getWritableDatabase();
+        Cursor c = sqDb.query(HelperDB.tableName, null,null, null, null, null, null);
+        int col1 = c.getColumnIndex(HelperDB.login);
+        int col2 = c.getColumnIndex(HelperDB.passw);
+        c.moveToFirst();
+        while (!c.isAfterLast())
+        {
+            String t1 = c.getString(col1);
+            String t2 = c.getString(col2);
+            if (strLogin.equals(t1) && strPassword.equals(t2))
+            {
+                flag = true;
+                break;
+            }
+            c.moveToNext();
+        }
+        c.close();
+        sqDb.close();
+        return flag;
+
     }
 }
